5142e89f608620a724d2ec4950af70faf1c320a8
saltedFish <1104029432@qq.com>
2023-10-22 00:49:46 +0800

    sentinel引入及docker-compose部署

--- cloud-test/order-service/src/main/java/cn/itcast/order/web/OrderController.java
+++ cloud-test/order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -2,6 +2,7 @@ package cn.itcast.order.web;
 
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.service.OrderService;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,5 +20,17 @@ public class OrderController {
     public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
         // 根据id查询订单并返回
         return orderService.queryOrderById(orderId);
+    }
+
+    @SentinelResource
+    @GetMapping("query")
+    public String queryOrder() {
+        return "查询订单成功";
+    }
+
+    @SentinelResource
+    @GetMapping("update")
+    public String updateOrder() {
+        return "更新订单成功";
     }
 }
