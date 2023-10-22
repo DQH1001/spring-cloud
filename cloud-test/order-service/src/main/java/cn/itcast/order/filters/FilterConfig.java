c0ed68c9e7dc75f8e133570f9df71c7f89a41cbb
saltedFish <1104029432@qq.com>
2023-10-22 22:49:48 +0800

    sentinel链路模式-依赖问题改为另起一个项目实验

---
+++ cloud-test/order-service/src/main/java/cn/itcast/order/filters/FilterConfig.java
@@ -0,0 +1,24 @@
+package cn.itcast.order.filters;
+
+import com.alibaba.cloud.sentinel.SentinelProperties;
+import com.alibaba.csp.sentinel.adapter.servlet.CommonFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+import java.util.Collections;
+
+@Configuration
+public class FilterConfig {
+	@Bean
+	public FilterRegistrationBean sentinelFilterRegistration(){
+		FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
+		registration.setFilter(new CommonFilter());
+		registration.setUrlPatterns(Collections.singleton("/*"));
+		registration.setName("sentinelFilter");
+		registration.setOrder(1);
+		return registration;
+	}
+}
--- cloud-test/order-service/src/main/java/cn/itcast/order/web/OrderController.java
+++ cloud-test/order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -2,7 +2,6 @@ package cn.itcast.order.web;
 
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.service.OrderService;
-import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,24 +12,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("order")
 public class OrderController {
 
-   @Autowired
-   private OrderService orderService;
+	@Autowired
+	private OrderService orderService;
 
-    @GetMapping("{orderId}")
-    public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
-        // 根据id查询订单并返回
-        return orderService.queryOrderById(orderId);
-    }
+	@GetMapping("{orderId}")
+	public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
+		// 根据id查询订单并返回
+		return orderService.queryOrderById(orderId);
+	}
 
-    @SentinelResource
-    @GetMapping("query")
-    public String queryOrder() {
-        return "查询订单成功";
-    }
+	@GetMapping("query")
+	public String queryOrder() {
+		orderService.queryGoods();
+		return "查询订单成功";
+	}
 
-    @SentinelResource
-    @GetMapping("update")
-    public String updateOrder() {
-        return "更新订单成功";
-    }
+	@GetMapping("update")
+	public String updateOrder() {
+		return "更新订单成功";
+	}
+
+	@GetMapping("save")
+	public String saveOrder() {
+		orderService.queryGoods();
+		return "新增订单成功";
+	}
 }
