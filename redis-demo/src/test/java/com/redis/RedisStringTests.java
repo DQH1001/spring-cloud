28e4335825b71b57770f2a269b1e64a36966b48a
ad钙 <1104029432@qq.com>
2023-10-29 21:24:33 +0800

    redis快速入门

---
+++ redis-demo/src/test/java/com/redis/RedisStringTests.java
@@ -0,0 +1,37 @@
+package com.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.Map;
+
+@SpringBootTest
+class RedisStringTests {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    void testString() {
+        // 写入一条String数据
+        stringRedisTemplate.opsForValue().set("name", "124143");
+        // 获取string数据
+        Object name = stringRedisTemplate.opsForValue().get("name");
+        System.out.println("name = " + name);
+    }
+
+    @Test
+    void testHash() {
+        stringRedisTemplate.opsForHash().put("user:400", "name", "虎哥");
+        stringRedisTemplate.opsForHash().put("user:400", "age", "21");
+
+        Map<Object, Object> entries = stringRedisTemplate.opsForHash().entries("user:400");
+        System.out.println("entries = " + entries);
+    }
+
+    @Test
+    void name() {
+    }
+}
