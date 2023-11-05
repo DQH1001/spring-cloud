172892812b543a1ff98f950c2ae693d43edbda63
ad钙 <1104029432@qq.com>
2023-11-05 18:10:46 +0800

    redis分片集群

--- redis-demo/src/main/java/cn/itcast/redisdemo/RedisDemoApplication.java
+++ redis-demo/src/main/java/cn/itcast/redisdemo/RedisDemoApplication.java
@@ -1,11 +1,10 @@
-package com.heima;
+package cn.itcast.redisdemo;
 
 import io.lettuce.core.ReadFrom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 
 @SpringBootApplication
 public class RedisDemoApplication {
